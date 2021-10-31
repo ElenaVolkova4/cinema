eea86cc81b88a66a8cdff315fe1167525d5b6e45
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2021-10-31 22:42:11 +0300

    Сделала адаптив, пока не работает полоса прокрутки

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,5 +1,8 @@
 const schemeSvg = document.querySelector('.scheme-svg');
 const totalPriceTag = document.querySelector('.price-total');
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.menu');
+
 let cost = 800;
 let totalPrice = 0;
 
@@ -17,5 +20,10 @@ if (!event.target.classList.contains('booked')) { //не занято ли эт�
   totalPriceTag.textContent = totalPrice;
 }
 
+
+});
+
+menuButton.addEventListener('click', () => {
+menu.classList.toggle('is-open');
 
 })
